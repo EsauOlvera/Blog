e61c8242e9608ed9e4b9f67c5bd42bbfdc267f31
edolvera <esau.o.satoritech@gmail.com>
2024-01-18 15:08:18 -0600

    Navigation added
    Added navigation, tailwicss, and yarn

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {
   View,
+  Text
 } from 'react-native';
+import AppNavigation from './src/navigation/Navigation';
 
 function App(){
 
   return (
-    <View></View>
+    <AppNavigation />
   );
 }
 
